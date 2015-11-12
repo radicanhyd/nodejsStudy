57bd7d70d4eb1bb9f9449bb1daf990d36cbcea6b
radicanhyd <radicanhyd@radicanhyd>
2015-11-13 01:32:23 +0900

    model exports test

--- node-rest-basic/routes/index.js
+++ node-rest-basic/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+/* model test */
+var model = require('../model/data');
+
+var mylist = [{label: "one", num: 1},{label: "two", num: 2},{num: 3}] ;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	
+	// model getdata
+	model.getData(mylist);
+	
+	res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
